f69137171b68fb39b42fdd52ad7d420fecf1d715
Federico Croci <fedecroci@hotmail.com>
2021-08-14 17:28:05 -0300

    Basic gRPC server and client.

---
+++ gRPC/gRPC.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Comm;
+using Grpc.Core;
+
+namespace gRPC.Client
+{
+	class Program
+	{
+		const string ServerHost = "localhost";
+		const int ServerPort = 50008;
+
+		static void Main(string[] args)
+		{
+			try
+			{
+				var channel = new Channel($"{ServerHost}:{ServerPort}", ChannelCredentials.Insecure);
+				channel.ConnectAsync().ContinueWith((task) =>
+				{
+					if (task.Status == System.Threading.Tasks.TaskStatus.RanToCompletion) {
+						Console.WriteLine($"Client connected to {ServerHost}:{ServerPort}!");					
+					}
+				});
+				var client = new Operations.OperationsClient(channel);
+				channel.ShutdownAsync().Wait();
+				Console.ReadKey();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Client error: {ex.Message}");
+			}
+		}
+	}
+}
---
+++ gRPC/gRPC.Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace gRPC.Server
+{
+	class Program
+	{
+		const int Port = 50008;
+		static void Main(string[] args)
+		{
+			Grpc.Core.Server server = null;
+			try
+			{
+				server = new Grpc.Core.Server()
+				{
+					Ports = { new Grpc.Core.ServerPort("localhost", Port,Grpc.Core.ServerCredentials.Insecure)
+					}
+				};
+				server.Start();
+				Console.WriteLine($"Server started on port {Port}!");
+				Console.ReadKey();
+			}
+			catch (IOException ex)
+			{
+				Console.WriteLine($"Server error:{ex.Message}");
+
+			}
+			finally {
+				if(server != null) server.ShutdownAsync().Wait();
+				Console.WriteLine("Server stopped!");
+			}
+		}
+	}
+}
